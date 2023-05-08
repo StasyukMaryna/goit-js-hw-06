f706926f1e1e87acc3c9a7c4eab4de664d2410a3
StasyukMaryna <stasyuk.m.v@gmail.com>
2023-05-08 23:08:12 +0200

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const list = document.querySelector("ul#ingredients");
+
+const listCreate = ingredients.forEach((el) => {
+  const listIng = document.createElement("li");
+  listIng.textContent = el;
+  listIng.classList.add("item");
+  list.appendChild(listIng);
+  console.log(listIng);
+});
