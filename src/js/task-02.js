5975d85d00c498d40248ed74b1469051648372c7
StasyukMaryna <stasyuk.m.v@gmail.com>
2023-05-22 18:22:52 +0200

    1

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -8,9 +8,11 @@ const ingredients = [
 ];
 const ul = document.querySelector("#ingredients");
 
-ingredients.forEach((ingredient) => {
+const listItems = ingredients.map((ingredient) => {
   const li = document.createElement("li");
   li.textContent = ingredient;
   li.classList.add("item");
-  ul.appendChild(li);
+  return li;
 });
+
+ul.append(...listItems);
